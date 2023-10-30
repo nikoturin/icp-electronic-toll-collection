60b82e862a14365dc477cde073b12c8e4ca757d4
root <root@NIN405.DOMNAVINSA.NET>
2023-10-30 04:38:44 -0600

    Add call to insert in principal storage

--- etc-stack/etc-client/etc_agent.py
+++ etc-stack/etc-client/etc_agent.py
@@ -6,11 +6,23 @@ from ic.candid import Types, encode
 
 client = Client(url = "http://127.0.0.1:4943")
 
-iden = Identity(privkey="6af3ca5313e053186f3f45919bad90c4a64bd5cfe235cb0d5e2176c104a00cce")
+iden = Identity(privkey="35be4e5b9004d228fdf77e3d70e4e7d4824a0cf4ab901d6e9f3e33aa6e46734b")
+
 print('principal:', Principal.self_authenticating(iden.der_pubkey))
 ag = Agent(iden, client)
 print(ag)
 start = time.time()
 
-ret = ag.update_raw("bkyz2-fmaaa-aaaaa-qaaaq-cai", "greet", encode([{'type': Types.Text,'value':'Okokok'}]))
-print('greet:', ret)
+#'("1",record { Ope = "OPE1"; StrToll = "OPE2WWWXXXYYYZZZ"; Status = "N" })'
+
+record = Types.Record({ 'Ope': Types.Text, 'StrToll': Types.Text, 'Status': Types.Text })
+
+ret = ag.update_raw("bkyz2-fmaaa-aaaaa-qaaaq-cai", "insert", encode([
+
+        {'type': Types.Text,'value': '6'}, 
+        {'type': record, 'value':
+            {'Ope':'OPE1','StrToll':'OPE2WWWXXXYYYZZZ','Status':'N'}
+        }
+    ]))
+
+print('etc_db insert:', ret)
